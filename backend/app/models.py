38baa6449f96432c502f2f4217b6fb5d77c989ea
unknown <kathanmajithia@gmail.com>
2025-07-12 11:06:29 +0530

    Added libraries

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,7 @@
 from app.database import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import (Boolean,Column,String,DateTime,Integer,Enum,Table,Text,ForeignKey)
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import declarative_base,relationship
+import uuid
+from datetime import datetime
 
