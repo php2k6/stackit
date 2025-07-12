41c7dc55cf2d6ef668606e90b9813249cfbadf93
unknown <kathanmajithia@gmail.com>
2025-07-12 11:10:40 +0530

    Question tags

--- backend/app/models.py
+++ backend/app/models.py
@@ -5,3 +5,12 @@ from sqlalchemy.orm import declarative_base,relationship
 import uuid
 from datetime import datetime
 
+Base = declarative_base()
+
+ques_tag = Table(
+    "ques_tag",
+    Base.metadata,
+    Column("qid",UUID(as_uuid=True),ForeignKey("questions.qid"),primary_key=True),
+    Column("tag",String,primary_key=True)
+)
+
